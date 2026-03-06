9a11b6f7849a4eb66062766a5703f4d7d60c195a
alvianzf <alvianzf@gmail.com>
2026-03-06 14:47:34 +0700

    feat: enhance book entries with additional properties

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -14,12 +14,36 @@ type Book = {
   id: string;
   title: string;
   author: string;
+  pages: number;
+  isAvailable: boolean;
+  publishedDate: Date;
 };
 
 const books: Book[] = [
-  { id: '1', title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
-  { id: '2', title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-  { id: '3', title: '1984', author: 'George Orwell' },
+  {
+    id: '1',
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    pages: 180,
+    isAvailable: true,
+    publishedDate: new Date('1925-04-10'),
+  },
+  {
+    id: '2',
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    pages: 281,
+    isAvailable: false,
+    publishedDate: new Date('1960-07-11'),
+  },
+  {
+    id: '3',
+    title: '1984',
+    author: 'George Orwell',
+    pages: 328,
+    isAvailable: true,
+    publishedDate: new Date('1949-06-08'),
+  },
 ];
 
 @Controller('books')
