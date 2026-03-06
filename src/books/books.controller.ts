7e02040bf3ce5ec55e44b8f5826f45c6e3a0ab04
alvianzf <alvianzf@gmail.com>
2026-03-06 14:29:36 +0700

    feat: populate books CRUD with mock returns

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  // Body,
+} from '@nestjs/common';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  // implement books CRUD here
+  @Get()
+  findAll() {
+    return 'All books';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Book with id ${id}`;
+  }
+
+  @Post()
+  create() {
+    return 'Book created';
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `Book with id ${id} updated`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Book with id ${id} deleted`;
+  }
+}
