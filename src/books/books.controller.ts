2e0210b301f3681bfe39caaed8ec3016ccdaa70d
alvianzf <alvianzf@gmail.com>
2026-03-06 14:33:19 +0700

    feat: sample endpoint with static data

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -5,34 +5,79 @@ import {
   Put,
   Delete,
   Param,
-  // Body,
+  Body,
 } from '@nestjs/common';
+
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+};
+
+const books: Book[] = [
+  { id: '1', title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  { id: '2', title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+];
 
 @Controller('books')
 export class BooksController {
   // implement books CRUD here
   @Get()
   findAll() {
-    return 'All books';
+    return {
+      message: 'List of all books',
+      data: books,
+    };
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `Book with id ${id}`;
+    const book = books.find((b) => b.id === id);
+    if (!book) {
+      return {
+        message: 'Book not found',
+      };
+    }
+    return {
+      message: 'Book found',
+      data: book,
+    };
   }
 
   @Post()
-  create() {
-    return 'Book created';
+  create(@Body() bodyData: Book) {
+    books.push(bodyData);
+    return {
+      message: 'Book created',
+      data: bodyData,
+    };
   }
 
   @Put(':id')
   update(@Param('id') id: string) {
-    return `Book with id ${id} updated`;
+    const bookIndex = books.findIndex((b) => b.id === id);
+    if (bookIndex === -1) {
+      return {
+        message: 'Book not found',
+      };
+    }
+    return {
+      message: 'Book updated',
+      data: books[bookIndex],
+    };
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `Book with id ${id} deleted`;
+    const bookIndex = books.findIndex((b) => b.id === id);
+    if (bookIndex === -1) {
+      return {
+        message: 'Book not found',
+      };
+    }
+    books.splice(bookIndex, 1);
+    return {
+      message: 'Book deleted',
+    };
   }
 }
