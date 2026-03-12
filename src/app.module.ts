6ecf49c08bb08a6d1a6e21e33da120a24b5a703a
alvianzf <alvianzf@gmail.com>
2026-03-12 17:25:02 +0700

    docs: remove unused BooksController and BooksService from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksController } from './books/books.controller';
-import { BooksService } from './books/books.service';
 import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [BooksModule],
-  controllers: [AppController, BooksController],
-  providers: [AppService, BooksService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
