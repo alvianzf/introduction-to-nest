7f6cbbbb848f3a1bb03d24a1e6f2e219e54feb1a
alvianzf <alvianzf@gmail.com>
2026-03-15 16:58:52 +0700

    feat: add ValidationPipe options to main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,22 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  // Use Global Pipes for validation and transformation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strips away any properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throws an error if non-whitelisted properties are present
+      transform: true, // Automatically transforms payloads to be objects typed according to their DTO classes
+    }),
+  );
 
   const config = new DocumentBuilder()
-    .setTitle('Books API')
-    .setDescription('The unofficial Books API description')
-    .setVersion('1.0')
+    .setTitle('Introduction to NestJS')
+    .setDescription('Learning DTOs, Pipes, and API Documentation')
+    .setVersion('1.1')
+    .addTag('books')
+    .addTag('products')
+    .addTag('users')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
