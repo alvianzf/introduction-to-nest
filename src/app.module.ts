fa6ea36c7e0f53d91d7b9e6e335e66659516c94c
alvianzf <alvianzf@gmail.com>
2026-03-15 17:41:56 +0700

    Day 6 Step 3: Register LoggerMiddleware globally in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { ProductsModule } from './products.module';
 import { UsersModule } from './users.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [BooksModule, ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
